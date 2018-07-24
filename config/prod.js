11b7351cb69b267a9995a56acec414f9e9f29c4c
DESKTOP-ALI0L0F\abrar <achm.abrar@gmail.com>
2018-07-24 18:06:29 +0700

    finsihed auth flow 2

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,3 @@ module.exports = {
     cookieKey:process.env.COOKIE_KEY
 };
 
-//googleClientID :'181571194585-m0vg7k6k698r593tukm0toneunmj4emd.apps.googleusercontent.com',
-//googleClientSecret:'qIuEAJBjNBoVCwKEQ_iJ98gA',
-//mongooseUri:'mongodb://user-prod:p4$$1234@ds147181.mlab.com:47181/react-app-deb-prod',
-//cookieKey:'hdgsgdshdghsdghsdghssakagdakfkywtwewpqhxbzzmnvsgtreapd'

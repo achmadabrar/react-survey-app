27a596c8ab18fc692c935b2d72ce4d41fbad0a73
DESKTOP-ALI0L0F\abrar <achm.abrar@gmail.com>
2018-07-25 09:09:43 +0700

    perbaikan proxy

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,7 @@ module.exports = {
     googleClientID:process.env.GOOGLE_CLIENT_ID,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongooseUri:process.env.MONGOOSE_URI,
-    cookieKey:process.env.COOKIE_KEY
+    cookieKey:process.env.COOKIE_KEY,
+    googleRedirectURI:'https://react-survey-app.herokuapp.com/'
 };
 
